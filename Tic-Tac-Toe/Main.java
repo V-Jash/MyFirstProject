dc1aca731e1a1fe758f035b007e80b85d4a3cfa3
V-Jash <jashwanthvallabhaneni@gmail.com>
2025-08-30 14:54:09 +0530

    I have just added a comment to my file

---
+++ Tic-Tac-Toe/Main.java
@@ -0,0 +1,54 @@
+import javax.print.event.PrintEvent;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintBoard pb = new PrintBoard();
+        Evaluater ev = new Evaluater();
+        String winner = null;
+        String turn = "X";
+        String[] board = new String[9];
+        for (int i = 0; i < 9; i++) {
+            board[i] = String.valueOf(i + 1);
+        }
+        System.out.println("Welcome to the 3X3 Tic Tac Toe Game.");
+        pb.printBoard(board);
+        System.out.println("'X' will start first. And the game continues alternatively :");
+
+        while (winner == null) {
+            int inputNum;
+            try {
+                inputNum = sc.nextInt();
+                // checking the number is in a valid range
+                if (!(inputNum > 0 && inputNum <= 9)) {
+                    System.out.println("Enter a valid slot in the board:");
+                    continue;
+                }
+
+                if (board[inputNum - 1].equals(String.valueOf(inputNum))) {
+                    board[inputNum - 1] = turn;
+
+                    turn = turn.equals("X") ? "O" : "X";
+                    pb.printBoard(board);
+                    winner = ev.checkWinner(board, turn);
+                } else {
+                    System.out.println("The slot is already taken. Please Re-enter the slotNumber");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Enter a valid number to place your position.");
+                e.printStackTrace();
+                sc.nextLine();
+            }
+
+        }
+        if (winner.equalsIgnoreCase("DRAW")) {
+            System.out.println("It's a Draw.");
+
+        } else {
+            System.out.println(winner + " has won the match. Congratulations !!!");
+        }
+        sc.close();
+    }
+}

0043ab6d2cd6fa2223b39cd2c64f191955de1604
V-Jash <jashwanthvallabhaneni@gmail.com>
2025-09-02 23:42:31 +0530

    Finally created a file which can be executed on Web.

--- Tic-Tac-Toe/web-tictactoe/src/main/java/com/example/tictactoe/WebTicTacToeApplication.java
+++ Tic-Tac-Toe/web-tictactoe/src/main/java/com/example/tictactoe/WebTicTacToeApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicTacToeApplication {
+public class WebTicTacToeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TicTacToeApplication.class, args);
+        SpringApplication.run(WebTicTacToeApplication.class, args);
     }
 }
